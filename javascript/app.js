4d1e96df5a7d7cbdcea585eeb259114ed07e2c20
aCoder <67935554+aCoder-world@users.noreply.github.com>
2021-05-25 07:42:40 +0530

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,31 @@
+
+//client slider
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: '.slider-dots',
+    dotsClass: 'dots'
+  });
+});
+//travel slider
+$(document).ready(function () {
+  $('.travel-slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: '.travel-slider-dots',
+    dotsClass: 'dots'
+  });
+});
+
+// hamberger
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+
+hamberger.addEventListener('click', function () {
+  mobileNav.classList.add('open');
+});
+times.addEventListener('click', function () {
+  mobileNav.classList.remove('open');
+});
